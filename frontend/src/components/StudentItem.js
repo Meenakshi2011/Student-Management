bd1162fc02ae736a376db89be352fd0e979d2516
Meenakshi2011 <meenakshigoli2021@gmail.com>
2025-04-27 11:53:05 +0530

    Update StudentItem.js

--- frontend/src/components/StudentItem.js
+++ frontend/src/components/StudentItem.js
@@ -1,4 +1,4 @@
-import React from 'react'; 
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function StudentItem({ student, onDelete }) {
@@ -9,17 +9,17 @@ function StudentItem({ student, onDelete }) {
       <td>{student.email}</td>
       <td>{student.phone}</td>
       <td>{student.enrollmentNo}</td>
-      <td>{student.rollNo}</td> {/* Added roll number field */}
+      <td>{student.rollNo}</td> {/* Display roll number */}
       <td>{student.course}</td>
       <td>{student.active ? 'Yes' : 'No'}</td>
       <td>{student.dateOfBirth ? new Date(student.dateOfBirth).toLocaleDateString() : ''}</td>
       <td>
-        <Link to={`/edit-student/${student._id}`} className="btn btn-sm btn-pink text-white me-2">
+        <Link to={`/edit-student/${student._id}`} className="btn btn-sm btn-green me-2">
           Edit
         </Link>
         <button 
           onClick={() => onDelete(student._id)} 
-          className="btn btn-sm btn-pink text-white"
+          className="btn btn-sm btn-green"
         >
           Delete
         </button>
